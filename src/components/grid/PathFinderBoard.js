d6c7c94c8e5bd38a930f34682c018e601470fde8
Michael <osujim2012@gmail.com>
2021-11-10 17:02:54 -0500

    Add NodeStates, start and end nodes

--- src/components/grid/PathFinderBoard.js
+++ src/components/grid/PathFinderBoard.js
@@ -1,16 +1,31 @@
 import './PathFinderBoard.css';
 
+const NodeStates = {
+  start: 0,
+  end: 1,
+  visited: 2,
+  unvisited: 3,
+};
+
 class Node {
   constructor(x, y) {
     this.weight = 1;
     this.x = x;
     this.y = y;
+    this.nodeState = NodeStates.unvisited;
+  }
+  setState(state) {
+    const NodeDOM = document.getElementById(`${this.y}_${this.x}`);
+    switch (state) {
+      case NodeStates.start:
+        NodeDOM.className = 'node start';
+    }
   }
 }
 
 const PathFinderBoard = () => {
-  const width = 30;
-  const height = 15;
+  const width = 60;
+  const height = 30;
   let grid = new Array(height);
   for (let i = 0; i < height; i++) {
     let row = new Array(width);
@@ -19,13 +34,26 @@ const PathFinderBoard = () => {
     }
     grid[i] = row;
   }
+  console.log(grid);
+  const startNode = grid[~~(height / 2)][~~(width / 4)];
+  const endNode = grid[~~(height / 2)][width - ~~(width / 4)];
   return (
     <table className="board">
       <tbody>
         {grid.map((row) => (
           <tr className="row">
             {row.map((node) => (
-              <td className="node unvisited" id={`${node.y}_${node.x}`}></td>
+              <td
+                className={
+                  'node ' +
+                  (node === startNode
+                    ? 'start'
+                    : node === endNode
+                    ? 'end'
+                    : 'unvisited')
+                }
+                id={`${node.y}_${node.x}`}
+              ></td>
             ))}
           </tr>
         ))}
