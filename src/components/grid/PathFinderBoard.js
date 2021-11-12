6c5fd22067fa37939275f8b53cb816cb606edcf5
Michael <osujim2012@gmail.com>
2021-11-12 07:50:47 -0500

    Remove contextMenu. Make grid a state. Add remove walls on right click. Add set to keep track of walls.

--- src/components/grid/PathFinderBoard.js
+++ src/components/grid/PathFinderBoard.js
@@ -1,5 +1,5 @@
 import './PathFinderBoard.css';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const NodeStates = {
   start: 'start',
@@ -12,6 +12,7 @@ const NodeStates = {
 class Node {
   static draggedState = null;
   static revertPreviousNode = null;
+  static walls = new Set();
   constructor(x, y) {
     this.weight = 1;
     this.x = x;
@@ -38,15 +39,38 @@ class Node {
       this.setThisAsPrevious();
     }
     this.setState(Node.draggedState);
+    if (Node.draggedState === NodeStates.wall) {
+      Node.walls.add(this.getKey());
+    } else if (Node.draggedState === NodeStates.unvisited) {
+      Node.walls.delete(this.getKey());
+    }
   }
+  getKey() {
+    return `${this.y}_${this.x}`;
+  }
+  // e.button === 0: the left button is clicked
+  // e.button === 1: the middle button is clicked
+  // e.button === 2: the right button is clicked
+  // e.button === 3: the `Browser Back` button is clicked
+  // e.button === 4: the `Browser Forward` button is clicked
   click(event) {
+    const which = event.button;
     event.preventDefault();
-    Node.draggedState = this.nodeState;
-    if (this.nodeState === NodeStates.unvisited) {
-      Node.draggedState = NodeStates.wall;
-      this.setState(NodeStates.wall);
-    } else {
-      this.setThisAsPrevious(NodeStates.unvisited);
+    if (which === 0) {
+      Node.draggedState = this.nodeState;
+      if (this.nodeState === NodeStates.unvisited) {
+        Node.draggedState = NodeStates.wall;
+        this.setState(NodeStates.wall);
+        Node.walls.add(this.getKey());
+      } else {
+        this.setThisAsPrevious(NodeStates.unvisited);
+      }
+    } else if (which === 2) {
+      Node.draggedState = NodeStates.unvisited;
+      if (this.nodeState === NodeStates.wall) {
+        this.setState(NodeStates.unvisited);
+        Node.walls.delete(this.getKey());
+      }
     }
   }
   setThisAsPrevious(state = this.nodeState) {
@@ -54,15 +78,22 @@ class Node {
   }
   setState(state) {
     if (state === undefined) state = NodeStates.unvisited;
+    //why bug?
+
+    //if (state === this.state) return;
     this.NodeDOM.className = 'node ' + NodeStates[state];
     this.nodeState = state;
   }
 }
 
 const PathFinderBoard = () => {
-  window.addEventListener('mouseup', () => (Node.draggedState = null));
+  window.addEventListener('mouseup', () => {
+    Node.draggedState = null;
+    Node.revertPreviousNode = null;
+  });
   const width = 60;
   const height = 30;
+
   let grid = new Array(height);
   for (let i = 0; i < height; i++) {
     let row = new Array(width);
@@ -73,11 +104,12 @@ const PathFinderBoard = () => {
   }
   grid[~~(height / 2)][~~(width / 4)].nodeState = NodeStates.start;
   grid[~~(height / 2)][width - ~~(width / 4)].nodeState = NodeStates.end;
+  const [currentGrid, setGrid] = useState(grid);
 
   useEffect(() => {
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width; j++) {
-        const currentNode = grid[i][j];
+        const currentNode = currentGrid[i][j];
         currentNode.NodeDOM = document.getElementById(
           `${currentNode.y}_${currentNode.x}`
         );
@@ -87,8 +119,8 @@ const PathFinderBoard = () => {
 
   return (
     <table className="board">
-      <tbody>
-        {grid.map((row) => (
+      <tbody onContextMenu={(e) => e.preventDefault()}>
+        {currentGrid.map((row) => (
           <tr className="row">
             {row.map((node) => (
               <td
