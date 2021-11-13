443e80b37f923e4b6b09feb28aa4bf29c257ad42
Michael <osujim2012@gmail.com>
2021-11-13 07:49:59 -0500

    Add BoardNode

---
+++ src/components/grid/BoardNode.js
@@ -0,0 +1,78 @@
+import { NodeStates } from './NodeStates';
+
+export class BoardNode {
+  static draggedState = null;
+  static revertPreviousNode = null;
+  static walls = new Set();
+  constructor(x, y) {
+    this.weight = 1;
+    this.x = x;
+    this.y = y;
+    this.nodeState = NodeStates.unvisited;
+    this.setState = this.setState.bind(this);
+    this.click = this.click.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
+  }
+  mouseOver(event) {
+    if (
+      BoardNode.draggedState === null ||
+      this.nodeState === NodeStates.start ||
+      this.nodeState === NodeStates.end
+    )
+      return;
+    //if (this.nodeState === NodeStates.unvisited)
+
+    if (
+      BoardNode.draggedState !== NodeStates.wall &&
+      BoardNode.revertPreviousNode !== null
+    ) {
+      BoardNode.revertPreviousNode();
+      this.setThisAsPrevious();
+    }
+    this.setState(BoardNode.draggedState);
+    if (BoardNode.draggedState === NodeStates.wall) {
+      BoardNode.walls.add(this.getKey());
+    } else if (BoardNode.draggedState === NodeStates.unvisited) {
+      BoardNode.walls.delete(this.getKey());
+    }
+  }
+  getKey() {
+    return `${this.y}_${this.x}`;
+  }
+  // e.button === 0: the left button is clicked
+  // e.button === 1: the middle button is clicked
+  // e.button === 2: the right button is clicked
+  // e.button === 3: the `Browser Back` button is clicked
+  // e.button === 4: the `Browser Forward` button is clicked
+  click(event) {
+    const which = event.button;
+    event.preventDefault();
+    if (which === 0) {
+      BoardNode.draggedState = this.nodeState;
+      if (this.nodeState === NodeStates.unvisited) {
+        BoardNode.draggedState = NodeStates.wall;
+        this.setState(NodeStates.wall);
+        BoardNode.walls.add(this.getKey());
+      } else {
+        this.setThisAsPrevious(NodeStates.unvisited);
+      }
+    } else if (which === 2) {
+      BoardNode.draggedState = NodeStates.unvisited;
+      if (this.nodeState === NodeStates.wall) {
+        this.setState(NodeStates.unvisited);
+        BoardNode.walls.delete(this.getKey());
+      }
+    }
+  }
+  setThisAsPrevious(state = this.nodeState) {
+    BoardNode.revertPreviousNode = this.setState.bind(this, state);
+  }
+  setState(state) {
+    if (state === undefined) state = NodeStates.unvisited;
+    //why bug?
+
+    //if (state === this.state) return;
+    this.NodeDOM.className = 'node ' + NodeStates[state];
+    this.nodeState = state;
+  }
+}
